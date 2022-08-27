6bdcffa50ad3398fd6585e1b69184a71735c4618
yueunseo <eunseo3227@naver.com>
2022-08-27 12:03:52 +0900

    ocr 기능

---
+++ app/src/main/java/com/example/yourtree/CameraText.java
@@ -0,0 +1,211 @@
+package com.example.yourtree;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CameraText extends AppCompatActivity {
+
+    Bitmap image; //사용되는 이미지
+    ImageView imagebackview; // 사진이 출력되는 이미지뷰
+    private TessBaseAPI mTess; //Tess API reference
+    String datapath = "" ; //언어데이터가 있는 경로
+
+    Button btn_ocr; //텍스트 추출 버튼
+
+    private String imageFilePath; //이미지 파일 경로
+    private Uri p_Uri;
+
+    static final int REQUEST_IMAGE_CAPTURE = 672;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_text);
+        btn_ocr = (Button)findViewById(R.id.ocrButton);
+        imagebackview = (ImageView) findViewById(R.id.imageView);
+
+        //언어파일 경로
+        datapath = getFilesDir()+ "/tesseract/";
+
+        //트레이닝데이터가 카피되어 있는지 체크
+        checkFile(new File(datapath + "tessdata/"), "kor");
+        checkFile(new File(datapath + "tessdata/"), "eng");
+
+
+        /**
+         * Tesseract API
+         * 한글 + 영어(함께 추출)
+         * 한글만 추출하거나 영어만 추출하고 싶다면
+         * String lang = "eng"와 같이 작성해도 무관
+         **/
+        String lang = "kor+eng";
+
+        mTess = new TessBaseAPI();
+        mTess.init(datapath, lang);
+
+        // imagedown에서 사진 전달 받기
+        String geturi = getIntent().getStringExtra("uri");
+        if (geturi != null && !geturi.isEmpty()) {
+            Uri imageUri = Uri.parse(geturi);
+            imagebackview.setImageURI(imageUri);
+        }
+
+        // 텍스트 추출 버튼
+        btn_ocr.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                // 가져와진 사진을 bitmap으로 추출
+                BitmapDrawable d = (BitmapDrawable)((ImageView) findViewById(R.id.imageView)).getDrawable();
+                image = d.getBitmap();
+
+                String OCRresult = null;
+                mTess.setImage(image);
+
+                //텍스트 추출
+                OCRresult = mTess.getUTF8Text();
+                TextView OCRTextView = (TextView) findViewById(R.id.OCRTextView);
+                OCRTextView.setText(OCRresult);
+            }
+        });
+    }
+
+    // 사진의 돌아간 각도를 계산하는 메서드 선언
+    private int exifOrientationToDegrees(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
+
+    // 이미지를 회전시키는 메서드 선언
+    private Bitmap rotate(Bitmap bitmap, float degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            ((ImageView) findViewById(R.id.imageView)).setImageURI(p_Uri);
+            ExifInterface exif = null;
+
+            Bitmap bitmap = BitmapFactory.decodeFile(imageFilePath);
+            try {
+                exif = new ExifInterface(imageFilePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            int exifOrientation;
+            int exifDegree;
+
+            if (exif != null) {
+                exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                exifDegree = exifOrientationToDegrees(exifOrientation);
+            } else {
+                exifDegree = 0;
+            }
+            ((ImageView)findViewById(R.id.imageView)).setImageBitmap(rotate(bitmap, exifDegree));
+        }
+    }
+
+    // ImageFile의 경로를 가져올 메서드 선언
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "TEST_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,      /* prefix */
+                ".jpg",         /* suffix */
+                storageDir          /* directory */
+        );
+        imageFilePath = image.getAbsolutePath();
+        return image;
+    }
+
+    //장치에 파일 복사
+    private void copyFiles(String lang) {
+        try{
+            //파일이 있을 위치
+            String filepath = datapath + "/tessdata/"+lang+".traineddata";
+
+            //AssetManager에 액세스
+            AssetManager assetManager = getAssets();
+
+            //읽기/쓰기를 위한 열린 바이트 스트림
+            InputStream instream = assetManager.open("tessdata/"+lang+".traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            //filepath에 의해 지정된 위치에 파일 복사
+            byte[] buffer = new byte[1024];
+            int read;
+
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //check file on the device
+    private void checkFile(File dir, String lang) {
+        //디렉토리가 없으면 디렉토리를 만들고 그후에 파일을 카피
+        if(!dir.exists()&& dir.mkdirs()) {
+            copyFiles(lang);
+        }
+        //디렉토리가 있지만 파일이 없으면 파일카피 진행
+        if(dir.exists()) {
+            String datafilepath = datapath+ "/tessdata/"+lang+".traineddata";
+            File datafile = new File(datafilepath);
+            if(!datafile.exists()) {
+                copyFiles(lang);
+            }
+        }
+    }
+}
